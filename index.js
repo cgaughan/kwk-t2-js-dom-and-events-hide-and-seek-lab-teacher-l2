7d4d8881d748d3c41007f2c62ddcf319ce496969
Caroline Gaughan <cgaughan1122@gmail.com>
2018-06-21 13:33:30 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.getElementById("nested").querySelector("div.target")
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('ul.ranked-list');
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = Number.parseInt((i + 1).toString());
+  }
+}
+
+function deepestChild () {
+  return document.querySelector("div#grand-node div div div div")
+}
